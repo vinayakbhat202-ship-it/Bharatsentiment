6e128d2d6824563ca0780bbe217718de742247b9
Vinayak <your@email.com>
2026-06-10 05:24:18 +0530

    fix: point to Railway API

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-API = "https://vinny2005-bharatsentiment.hf.space"
+API =  "https://bharatsentiment-production.up.railway.app"
 
 st.set_page_config(
     page_title="BharatSentiment Pro",
